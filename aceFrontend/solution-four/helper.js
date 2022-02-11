54825c58e1679d8e47ec83f504ba6dfd03cec195
Berkeley V-H <berkeleyvh20@gmail.com>
2022-02-11 22:56:52 +0000

    error validation

--- aceFrontend/solution-four/helper.js
+++ aceFrontend/solution-four/helper.js
@@ -1,1 +1,21 @@
-console.log("H")
+const emailReg = /[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\\.[a-z0-9-]+)*/i
+
+const form = document.getElementById('form')
+form.addEventListener('change', validateForm)
+
+function validateForm(){
+    const error = []
+    if(!form.name.value){
+        error.push('Name is required')
+    }
+    if(!emailReg.test(form.email.value)){
+        error.push('Email must be a valid email format')
+    }
+    if(!form.password.value || form.password.value.length < 6){
+        error.push('Password must be 6 characters or greater')
+    }
+    if(form.confirm.value != form.password.value){
+        error.push('Password confirm field should match original password')
+    }
+    return error
+}
