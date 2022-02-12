37d79c6990bf0d8d63edfec34dcf206e34babae1
Berkeley V-H <berkeleyvh20@gmail.com>
2022-02-12 00:22:54 +0000

    vanilla js form complete

--- aceFrontend/solution-four/helper.js
+++ aceFrontend/solution-four/helper.js
@@ -4,18 +4,34 @@ const form = document.getElementById('form')
 form.addEventListener('change', validateForm)
 
 function validateForm(){
-    const error = []
+    const errors = []
     if(!form.name.value){
-        error.push('Name is required')
+        errors.push('Name is required')
     }
     if(!emailReg.test(form.email.value)){
-        error.push('Email must be a valid email format')
+        errors.push('Email must be a valid email format')
     }
     if(!form.password.value || form.password.value.length < 6){
-        error.push('Password must be 6 characters or greater')
+        errors.push('Password must be 6 characters or greater')
     }
     if(form.confirm.value != form.password.value){
-        error.push('Password confirm field should match original password')
+        errors.push('Password confirm field should match original password')
     }
-    return error
+    displayErrors(errors)
+}
+
+function displayErrors(err){
+
+    let submitButton = document.getElementById('submit')
+    if(err.length > 0 ){
+        submitButton.disabled = true
+    }else{
+        submitButton.disabled = false
+    }
+    document.getElementById('error__box').innerHTML = `
+        <ul id="error__list">
+            ${err.map(e => `<li> ${e} </li>`).join('')}
+        </ul>
+        `
+
 }
