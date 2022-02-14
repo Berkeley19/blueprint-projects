91a3202f5bd004adc534ac26b2dfc46209a128dc
Berkeley V-H <berkeleyvh20@gmail.com>
2022-02-14 11:37:39 +0000

    finish tab with class

--- aceFrontend/solution-five/helper.js
+++ aceFrontend/solution-five/helper.js
@@ -16,39 +16,41 @@ const tabObject = [
 //we need a grid
 //one section, with a grid of grid-column-template 45px, 1fr size of 300px
 //then inside first grid we have a flex 
+class Tabs {
+    constructor(el, tabsContent = [], tabsIndex = 0) {
+        el.innerHtml = ''
+        this.tabsIndex = tabsIndex
+        this.tabsContent = tabsContent
+        this.previous = []
 
-//for js, add eventlistener to each label tab, by 
+        const nav = document.createElement("nav")
+        const list = document.createElement("ul")
+        list.className = "tabs__list"
 
-const labels = document.getElementById('labels');
-let currentColor = []
+        tabsContent.forEach((tab, index) =>
+            list.insertAdjacentHTML('beforeend',
+                `
+            <li data-tab-index="${index}" class="tab__list-item">${tab.label}</li>
+            `
+            )
+        )
+        nav.addEventListener('click', this.handleClick)
+        nav.appendChild(list)
 
-for (let i = 0; i < labels.children.length; i++) {
-    console.log('pp')
-    labels.children[i].addEventListener("click", switchTabs);
-    console.log(labels.children[i]);
-}
+        this.content = document.createElement('article')
+        this.content.className = 'tabs__content'
+        this.content.innerHTML = tabsContent[tabsIndex] && tabsContent[tabsIndex].content
 
-function switchTabs(e) {
-    let contentWords = document.getElementById('content__words')
-    for (let i = 0; i < tabObject.length; i++) {
-        if (tabObject[i].label === e.target.textContent) {
-            contentWords.textContent = tabObject[i].content
-            if (currentColor.length === 0) {
-                currentColor.push(e.target.id)
-                document.getElementById(currentColor[0]).style.backgroundColor = "#0000ff80"
-            } else {
-                document.getElementById(currentColor[0]).style.backgroundColor = ""
-                currentColor.pop()
-                e.target.style.backgroundColor = "#0000ff80"
-                currentColor.push(e.target.id)
-            }
-            break
-        }
+        el.appendChild(nav)
+        el.appendChild(this.content)
+    }
+
+    handleClick = (e) => {
+        const i = e.target.dataset.tabIndex
+        this.content.innerHTML = this.tabsContent[i] && this.tabsContent[i].content
     }
 }
 
-
-
-
-
+const parent = document.getElementById("tabs")
+new Tabs(parent, tabObject)
 
